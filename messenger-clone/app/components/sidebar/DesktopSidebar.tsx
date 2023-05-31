678b8271f95388b4998b64d60f85fb9a4b43b227
kevinssheva <kevinssheva@gmail.com>
2023-06-01 00:00:22 +0700

    settings done
    settings is a feature for changing user name and profile pic

--- messenger-clone/app/components/sidebar/DesktopSidebar.tsx
+++ messenger-clone/app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
 import Avatar from "../Avatar";
+import SettingsModal from "./SettingsModal";
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -18,6 +19,11 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
 
   return (
     <>
+      <SettingsModal 
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
       <div className=" hidden justify-between lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:flex lg:w-20 lg:flex-col lg:overflow-y-auto lg:border-r-[1px] lg:bg-white lg:pb-4 xl:px-6">
         <nav className="mt-4 flex flex-col justify-between">
           <ul role="list" className="flex flex-col items-center space-y-1">
